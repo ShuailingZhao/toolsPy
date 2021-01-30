61a1b7b85d01ced6d97db7a224348ad3f2531adf
zhaoshuailing <zhaoshuailing@navinfo.com>
2021-01-30 19:06:28 +0800

    update the readwritepfm, so the .pfm file can use both in python and c++

--- testReadWritePfmFile.py
+++ testReadWritePfmFile.py
@@ -7,7 +7,7 @@ from showImageTools import showImage, waitMoment
 def main():
 	fpath="./test/0008_2.pfm"
 	img1 = readPfm1(fpath)
-	print("------------- ", img1[214,872])
+	print("--------------- ", img1[214,872])
 	showImg = img1.astype(np.uint8);
 
 	showImage(fpath, showImg)
